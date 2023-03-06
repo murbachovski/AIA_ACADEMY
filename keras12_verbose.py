ca5c3b4811f56ed935933d58fd004b853879ed2c
murbachovski <murbachovski@gmail.com>
2023-03-06 12:20:49 +0900

    DACON실데이터 측정

--- keras12_verbose.py
+++ keras12_verbose.py
@@ -10,18 +10,10 @@ y = dataset.target
 
 print(dataset)
 print(dataset.feature_names)
-# ['CRIM' 'ZN' 'INDUS' 'CHAS' 'NOX' 'RM' 'AGE' 'DIS' 'RAD' 'TAX' 'PTRATIO' 'B' 'LSTAT']
-# (1, 13) input_dim = 13
 
 print(dataset.DESCR)
 
 print(x.shape, y.shape) # (506, 13) (506, ) # Vactor = 1
-
-#######[실습]#######
-
-#1. train 0.7
-#2. R2 0.8 이상
-##################################################################################
 
 #2. MODEL
 import numpy as np
@@ -50,17 +42,14 @@ x_train, x_test, y_train, y_test = train_test_split(
     random_state=190
 )
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(x_train, y_train, epochs = 8000, batch_size = 50, verbose = 1)
+model.fit(x_train, y_train, epochs = 10, batch_size = 2, verbose="auto")
 
-#4. EVALUATE, PREDICT
-loss = model.evaluate(x_test, y_test)
-print("loss:", loss)
+# #4. EVALUATE, PREDICT
+# loss = model.evaluate(x_test, y_test)
+# print("loss:", loss)
 
 y_predict = model.predict(x_test)
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict) # GOOD, Y = wX + b, 보조지표
 print("R2_SCORE: ", r2)
 
-#r2 0.6
-#r2 0.65
-#r2 0.749
