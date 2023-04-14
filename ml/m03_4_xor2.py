55ba2a62f544c8e2c4e6486a1cc247ca38938328
murbachovski <murbachovski@gmail.com>
2023-04-14 14:53:53 +0900

    SVC

--- ml/m03_4_xor2.py
+++ ml/m03_4_xor2.py
@@ -1,14 +1,13 @@
 import numpy as np
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import Perceptron
+
 #1. DATA
 x_data = [[0,0], [0,1], [1,0], [1,1]]
 y_data = [0, 1, 1, 0] # 같으면 0 다르면 1
 
 #2. MODEL
-# model = LinearSVC()
-model = Perceptron()
+model = SVC()
 
 #3. COMPILE
 model.fit(x_data, y_data)
