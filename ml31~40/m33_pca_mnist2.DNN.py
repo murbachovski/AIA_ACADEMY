22ea3d0f6c813fb0077f6f02ab684f740feb9d5c
murbachovski <murbachovski@gmail.com>
2023-04-24 20:27:23 +0900

    Monday

--- ml31~40/m33_pca_mnist2.DNN.py
+++ ml31~40/m33_pca_mnist2.DNN.py
@@ -1,74 +1,43 @@
 from tensorflow.keras.datasets import mnist
 import numpy as np
 from sklearn.decomposition import PCA
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from tensorflow.keras.utils import to_categorical
-from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Dense, Input, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.python.keras.callbacks import EarlyStopping
-from sklearn.metrics import accuracy_score 
-import numpy as np
-from sklearn.metrics import accuracy_score 
+n_c_list = [154, 331, 486, 713]
+pca_list = [0.95, 0.99, 0.999, 1.0]
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data() # python 기초 문법
-# y는 뽑지 않고 x만 뽑아서 사용하겠다.
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# x = np.concatenate((x_train, x_test), axis=1)
 x = np.append(x_train, x_test, axis=0)
 y = np.append(y_train, y_test, axis=0)
+y = to_categorical(y)
+x = x.reshape(x.shape[0], -1)
 
-print(x.shape)
-print(y.shape)
+for i in range(len(n_c_list)):
+    pca = PCA(n_components=n_c_list[i])
+    x_p = pca.fit_transform(x.astype('float32'))
+    x_train, x_test, y_train, y_test = train_test_split(x_p, y, train_size=0.8, shuffle=True, random_state=123)
 
-x = x.reshape(x.shape[0], x.shape[1] * x.shape[2])
-temp = ['154', '331', '486', '713']
-# print(x.shape)
-for i in temp:
-    # perform PCA
-    pca = PCA(n_components=i)
-    x = pca.fit_transform(x)
-
-    # build and train model
-    model = Sequential([
-        Dense(64, input_shape=i,),
-        Dense(10, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-    model.fit(x, y, epochs=5)
-
-    # evaluate model on test data
-    y_pred = np.argmax(model.predict(x), axis=-1)
-    acc = accuracy_score(y_test, y_pred)
-    print('acc:', acc)
-
-#############################################
-#############################################
-
-# ACC :
-# 나의 최고의 CNN : acc: 0.7226999998092651
-# 나의 최고의 DNN : 
-# PCA 0.95  :
-# PCA 0.99  :
-# PCA 0.999 :
-# PCA 1.0   :
-
-
-
-
-
-# pca_EVR = pca.explained_variance_ratio_
-# # print(pca_EVR)
-
-# cumsum = np.cumsum(pca_EVR)
-# # print(np.cumsum(pca_EVR))
-
-# print('1.0', np.argmax(cumsum >= 1.0) + 1)
-# print('0.95', np.argmax(cumsum >= 0.999) + 1)
-# print('0.9', np.argmax(cumsum >= 0.99) + 1)
-# print('0.9', np.argmax(cumsum >= 0.95) + 1)
-# # 1.0 713
-# # 0.95 486
-# # 0.9 331
-# # 0.9 154
-
+    model = Sequential()
+    model.add(Dense(4, input_shape=(n_c_list[i],)))
+    model.add(Dense(4, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='acc')
+    model.fit(x_train, y_train, epochs=1, batch_size=16, validation_split=0.2)
+    
+    acc = model.evaluate(x_test, y_test)[1]
+    print(f'PCA {pca_list[i]} test acc : {acc}')
+    
+    y_pred = np.argmax(model.predict(x_test), axis=1)
+    print(f'PCA {pca_list[i]} pred acc :', accuracy_score(np.argmax(y_test, axis=1), y_pred))
+    
+# 최고 CNN : 0.3127893618736231
+# 최고 DNN : 0.3781293623792136
+# PCA 0.95 : 0.4563571512699127
+# PCA 0.99 : 0.5589285492897034
+# PCA 0.999 : 0.6482142806053162
+# PCA 1.0 : 0.6355000138282776
--- ml31~40/m34_pca_mnist3_xgb_gridSearch.py
+++ ml31~40/m34_pca_mnist3_xgb_gridSearch.py
@@ -1,67 +1,51 @@
-from tensorflow.keras.datasets import mnist
+# n_component > 0.95 이상
+# xgboost, gridSearch 또는 RandomSearch를 쓸 것
+
+# m33_2 결과 뛰어넘기
+
+parameters = [
+    {'n_estimators':[100, 200, 300], 'learning_rate':[0.1, 0.3, 0.001, 0.01],
+     'max_depth':[4, 5, 6]},
+    {'n_estimators':[90, 100, 110], 'learning_rate':[0.1, 0.001, 0.01],
+     'max_depth':[4, 5, 6], 'colsample_bytree':[0.6, 0.9, 1]},
+    {'n_estimators':[90, 110], 'learning_rate':[0.1, 0.001, 0.01],
+     'max_depth':[4, 5, 6], 'colsample_bytree':[0.6, 0.9, 1],
+     'colsample_bylevel':[0.6, 0.7, 0.9]},
+]
+
+# n_jobs = -1
+#     tree_method = 'gpu_hist'
+#     predictor = 'gpu_predictor'
+#     gpu_id = 0
+
 import numpy as np
-from sklearn.metrics import accuracy_score
-from xgboost import XGBClassifier as xgb
+from xgboost import XGBClassifier
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 from sklearn.decomposition import PCA
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.model_selection import KFold,cross_val_score, StratifiedKFold
-# n_component › 0.95
-# xgboost, gridsearch 또는 Randomsearch를 쓸것
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+
+n_c_list = [154, 331, 486, 713]
+pca_list = [0.95, 0.99, 0.999, 1.0]
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# print(x_train.shape, y_train.shape) # (60000, 28, 28) (60000,)
-# print(x.shape, y.shape) # (60000, 28, 28) (60000,)
+
 x = np.append(x_train, x_test, axis=0)
 y = np.append(y_train, y_test, axis=0)
-x = np.array(x)
-y = np.array(y)
-print(x.shape, y.shape) # (70000, 28, 28) (70000,)
-# x_train = np.array(x_train)
-x = x.reshape(70000, 28*28)
-# x_test = np.array(x_test)
-# x_test = x_test.reshape(60000, 28*28)
+# y = to_categorical(y)
+x = x.reshape(x.shape[0], -1)
 
-pca = PCA(n_components=10)
-x = pca.fit_transform(x)
+for i in range(len(n_c_list)):
+    pca = PCA(n_components=n_c_list[i])
+    x_p = pca.fit_transform(x.astype('float32'))
+    x_train, x_test, y_train, y_test = train_test_split(x_p, y, train_size=0.8, shuffle=True, random_state=123)
 
-# 결과를 뛰어넘어랏!!!
-xgb_parameters = [
-    {"_estimators": [100, 200, 300],
-     "learning_rate": [0.1, 0.3, 0.001, 0.01],
-    "max_depth": [4, 5, 6]},
-    {"_estimators": [90, 100, 110],
-    "learning_rate": [0.1, 0.001, 0.01],
-    "max _depth": [4,5,6],
-    "colsample_bytree": [0.6, 0.9, 1]},
-    {"_estimators": [90, 110],
-    "learning rate": [0.1, 0.001, 0.5],
-    "max _depth": [4,5,6],
-    "colsample _bytree": [0.6, 0.9, 1]},
-    {"colsample_bylevel": [0.6, 0.7, 0.9]}
-]
-
-
-n_splits = 5
-kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=337)
-
-RSCV_parameters = [
-    {'C': [1, 10, 100, 1000], 'kernel': ['linear'], 'degree': [3, 4, 5]}, # 12
-    {'C': [1, 10, 100], 'kernel': ['rbf, lienear'], 'gamma': [0.001, 0.0001]}, # 12
-    {'C': [1, 10, 100, 1000], 'kernel': ['sigmoid'], 'gamma': [0.01, 0.001, 0.0001], 'degree': [3, 4]}, # 24
-    {'C':[0.1, 1], 'gamma': [1,10]} # 4
-]
-
-#2. MODEl
-model = RandomizedSearchCV(xgb(),
-                    RSCV_parameters,
-                     cv=5,
-                     verbose=1,
-                     refit=True,
-                     n_jobs=-1
-)
-model.fit(x, y)
-
-y_pred = model.predict(x)
-acc = accuracy_score(y, y_pred)
-print('acc:', acc)
-# acc: 0.9863
+    model = GridSearchCV(XGBClassifier(tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0), parameters, cv=5, refit=True, n_jobs=-1)
+    model.fit(x_train, y_train)
+    
+    acc = model.evaluate(x_test, y_test)
+    print(f'PCA {pca_list[i]} test acc : {acc}')
+    
+    y_pred = np.argmax(model.predict(x_test), axis=1)
+    print(f'PCA {pca_list[i]} pred acc :', accuracy_score(np.argmax(y_test, axis=1), y_pred))
