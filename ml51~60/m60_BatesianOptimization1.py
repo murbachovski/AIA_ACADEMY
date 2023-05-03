a64901c2aeb493bcf032891653d57bf106178e68
murbachovski <murbachovski@gmail.com>
2023-05-03 10:01:34 +0900

    Bay

--- ml51~60/m60_BatesianOptimization1.py
+++ ml51~60/m60_BatesianOptimization1.py
@@ -4,3 +4,11 @@ param_bounds = {'x1' : (-1, 5),
 
 def y_function(x1, x2):
     return -x1 **2 - (x2 - 2) **2 + 10
+
+from bayes_opt import BayesianOptimization
+
+optimizer = BayesianOptimization(
+    f = y_function,
+    pbounds = param_bounds,
+    random_state=337
+)
