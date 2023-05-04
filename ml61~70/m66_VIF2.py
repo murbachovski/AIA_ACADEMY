26da3389fb2eb2f7b41ad01be16dbeac849cf7ee
murbachovski <murbachovski@gmail.com>
2023-05-04 17:59:29 +0900

    VIF_all

--- ml61~70/m66_VIF2.py
+++ ml61~70/m66_VIF2.py
@@ -5,62 +5,56 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.preprocessing import StandardScaler
 
 # 1. DATA
 datasets = fetch_california_housing()
 df = pd.DataFrame(datasets.data, columns=datasets.feature_names)
 df['target'] = datasets.target
-# print(df)
 
-# df.boxplot()
-# df.plot.box()
-# plt.show()
 
-# df.info()
-# print(df.describe())
-
-# df['Population'].plot.box()
-# plt.show()
-
-# df['Population'].hist(bins=50)
-# plt.show()
-
-# df['target'].hist(bins=50)
-# plt.show()
-
-# ##################################x_Populataion log change####################
-# df['Population']
 y = df['target']
 x = df.drop(['target'], axis=1)
 
-x['Population'] = np.log(x['Population'])
-# #######################################y log cnage###################
-y = np.log1p(y)
-#########################
+# 다중공선성
+vif = pd.DataFrame()
+vif['variables'] = x.columns
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+
+vif['VIF'] = [variance_inflation_factor(x_scaled, i) for i in range(x_scaled.shape[1])]
+# print(vif)
+#     variables       VIF
+# 0      MedInc  2.501295
+# 1    HouseAge  1.241254
+# 2    AveRooms  8.342786
+# 3   AveBedrms  6.994995
+# 4  Population  1.138125
+# 5    AveOccup  1.008324
+# 6    Latitude  9.297624
+# 7   Longitude  8.962263
+
+x = x.drop(['Latitude'], axis=1)
+x = x.drop(['Latitude', 'Logitude'], axis=1)
+
+print(x)
 
 x_train, x_test, y_train, y_test = train_test_split(
-    x,
-    y,
-    shuffle=True,
-    train_size=0.8,
-    random_state=377
+    x_scaled, y, shuffle=True, random_state=337, test_size=0.2, # stratify=y
 )
 
-# 2. MODEL
-model = RandomForestRegressor()
+scaler2 = StandardScaler()
+x_scaled2 = scaler2.fit_transform(x_train)
+x_test = scaler2.transform(x_test)
 
+#2. MODEL
+model = RandomForestRegressor(random_state=337)
+
+#3. COMPILE
 model.fit(x_train, y_train)
 
-
-#4 .COMPILE
-score = model.score(x_test, y_test)
-print("SCORE: ", score)
-
-r2_score(np.expm1(y_test), np.expm1(model.predict(y_test)))
-print("R2: ", r2_score)
-
-#LOG CHANGE
-# 전 SCORE:  0.8166830016159595
-# 후 SCORE:  0.817400729762068
-
-# SCORE:  0.8393462472079883
+#4. PREDICT
+results = model.score(x_test, y_test)
+print('results', results)
