089ee57a7f4d82b3377704b1a4c09e73ae68725d
murbachovski <murbachovski@gmail.com>
2023-05-12 16:29:14 +0900

    R2, MAE 뽑기

--- tf114/tf12_R2.py
+++ tf114/tf12_R2.py
@@ -1,21 +1,21 @@
 import tensorflow as tf
+import numpy as np
+from sklearn.metrics import r2_score, mean_absolute_error
+import warnings
 
 
-x_train = [1,2,3] # 1
-y_train = [1,2,3] # 2
+x_train = [1, 2, 3]  # Input data
+y_train = [1, 2, 3]  # Output data
 x = tf.compat.v1.placeholder(tf.float32)
 y = tf.compat.v1.placeholder(tf.float32)
 
 w = tf.compat.v1.Variable([10], dtype=tf.float32, name='weight')
 
 hypothesis = x * w
-loss = tf.reduce_mean(tf.square(hypothesis - y)) # mse
-
+loss = tf.reduce_mean(tf.square(hypothesis - y))  # Mean Squared Error (MSE)
 
 lr = 1
 gradient = tf.reduce_mean((x * w - y) * x)
-# gradient = tf.reduce_mean((hypothesis - y) * x)
-
 descent = w - lr * gradient
 update = w.assign(descent)  # w = w - lr * gradient
 
@@ -25,21 +25,18 @@ loss_history = []
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-for step in range(2):
-
-    _, loss_v, w_v = sess.run([update, loss, w], feed_dict={x:x_train, y:y_train})
+for step in range(21):
+    _, loss_v, w_v = sess.run([update, loss, w], feed_dict={x: x_train, y: y_train})
     print(step, '\t', loss_v, '\t', w_v)
 
     w_history.append(w_v)
     loss_history.append(loss_v)
+
+predicted_values = sess.run(hypothesis, feed_dict={x: x_train})
+r2 = r2_score(y_train, predicted_values)
+mae = mean_absolute_error(y_train, predicted_values)
+
+print("R2 Score:", r2)
+print("MAE:", mae)
+
 sess.close()
-print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ W_history')
-print(w_history)
-print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ loss_history')
-print(loss_history)
-
-# plt.scatter(w_history, loss_history)
-# plt.xlabel('Weight')
-# plt.ylabel('Loss')
-# plt.show()
-
