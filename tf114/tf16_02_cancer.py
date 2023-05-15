ea81f505a3dad935a8c059cb17e567766ad26c10
murbachovski <murbachovski@gmail.com>
2023-05-15 17:41:54 +0900

    sigmoid_cancer

--- tf114/tf16_02_cancer.py
+++ tf114/tf16_02_cancer.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from sklearn.datasets import load_diabetes
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
@@ -7,13 +7,13 @@ import numpy as np
 tf.compat.v1.set_random_seed(1253)
 
 # 1. DATA
-x = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
-y = [[0], [0], [0], [1], [1], [1]]
+x, y = load_breast_cancer(return_X_y=True)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1253)
 
-xp = tf.compat.v1.placeholder(tf.float32, shape=[None, 2])
+xp = tf.compat.v1.placeholder(tf.float32, shape=[None, x.shape[1]])
 yp = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random_normal([2, 1]), name='weight')
+w = tf.compat.v1.Variable(tf.compat.v1.random_normal([x.shape[1], 1]), name='weight')
 b = tf.compat.v1.Variable(tf.compat.v1.zeros([1]), name='bias')
 
 logits = tf.compat.v1.matmul(xp, w) + b
@@ -21,7 +21,6 @@ hypothesis = tf.compat.v1.sigmoid(logits)
 
 # 2-1. COMPILE
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=yp, logits=logits))
-# loss = -tf.reduce_mean(y*tf.log(hypothesis) + (1-y) * tf.log(1-hypothesis)) # binary_crossentropy
 optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
 train = optimizer.minimize(loss)
 
@@ -30,13 +29,12 @@ sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 epochs = 2001
 for step in range(epochs):
-    sess.run([loss, train], feed_dict={xp: x, yp: y})
+    sess.run([loss, train], feed_dict={xp: x_train, yp: y_train})
     if step % 20 == 0:
         print(step)
 
 # 4. PREDICT
-y_pred = sess.run(tf.round(hypothesis), feed_dict={xp: x})
-# y_pred = tf.cast(y_pred>0.5, dtype=tf.float32)
+y_pred = sess.run(tf.round(hypothesis), feed_dict={xp: x_test})
 
-accuracy = accuracy_score(y, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
