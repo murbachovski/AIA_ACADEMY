4256d11a9c8fd9bcc69d969950672c0b7e824b43
murbachovski <murbachovski@gmail.com>
2023-05-15 11:31:53 +0900

    mv2

--- tf114/tf13_mv1.py
+++ tf114/tf13_mv1.py
@@ -24,14 +24,23 @@ b = tf.compat.v1.Variable(tf.compat.v1.random_normal([1]))
 # 2. model
 hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
 
+loss = tf.reduce_mean(tf.square(hypothesis - y)) # mse
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(loss)
 # Calculation of R2 score
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-y_pred = sess.run(hypothesis, feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, y: y_data})
-r2 = r2_score(y_data, y_pred)
+# y_pred = sess.run(hypothesis, feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, y: y_data})
+# r2 = r2_score(y_data, y_pred)
 
-print("R2 Score:", r2)
+epochs = 2001
+for step in range(epochs):
+    cost_val, hy_val = sess.run([loss, train],
+                              feed_dict={x1:x1_data, x2:x2_data, x3:x3_data, y:y_data})
+    if step % 20 == 0:
+        print(epochs, 'loss:', cost_val)
 
 sess.close()
 
