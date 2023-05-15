307213d9bfabf0445b01723935394013aac335ea
murbachovski <murbachovski@gmail.com>
2023-05-15 13:03:44 +0900

    tf_실습

--- tf114/tf13_mv3.py
+++ tf114/tf13_mv3.py
@@ -1,49 +1,33 @@
 import tensorflow as tf
-from sklearn.metrics import r2_score, mean_squared_error # = MSE
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
 tf.compat.v1.set_random_seed(337)
 
-x_data = [[1,2,3],
-          [1,2,3]]
-y_data = [[1],
-          [2],
-          [3]]
-
+x_data = [[1, 2, 3], [1, 2, 3]]
+y_data = [[1], [2], [3]]
+x_data = x_data + [[1, 2, 3]]
 x = tf.compat.v1.placeholder(tf.float32, shape=[None, 3])
 y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
 
 w = tf.compat.v1.Variable(tf.compat.v1.random_normal([3, 1]), name='weight')
 b = tf.compat.v1.Variable(tf.compat.v1.random_normal([1]), name='bias')
 
-
-#2. MODEL
-# hypothesis = x * w + b
+# 2. MODEL
 hypothesis = tf.compat.v1.matmul(x, w) + b
 
-# x.shape = (5, 3)
-# y.shape = (5, 1)
-# hy = x * w + b
-#    = (5, 3) * w + b = (5, 1)
-# (5, 3) * (?, ?) = (5, 1) 답 : (3, 1)
-
-loss = tf.reduce_mean(tf.square(hypothesis)) # mse
-
-#3. COMPILE, Fit
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
-train = optimizer.minimize(loss)
+# 3. COMPILE, Fit
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-epochs = 2001
-for step in range(epochs):
-    cost_val, hy_val = sess.run([loss, w],
-                              feed_dict={x:x_data, y:y_data})
-    if step % 20 == 0:
-        print(epochs, 'loss:', cost_val)
+# Training
+sess.run(hypothesis, feed_dict={x: x_data})
 
-y_predict = x * hy_val
+# Get predicted values
+y_pred = sess.run(hypothesis, feed_dict={x: x_data})
 
-print(sess.run([hypothesis, w, b], feed_dict={x:x_data}))
-# print('mse: ', mean_squared_error(y_predict, y))
-print('r2: ', r2_score(y_predict, y))
-sess.close()
+# Calculate R2 score
+r2 = r2_score(y_data, y_pred)
+print("R2 Score:", r2)
+mse = mean_squared_error(y_data, y_pred)
+print("MSE Score:", mse)
