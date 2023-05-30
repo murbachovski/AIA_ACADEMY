b5590935466459c4cb4bd1b0bb90c127419e420e
murbachovski <murbachovski@gmail.com>
2023-05-30 10:19:45 +0900

    AutoEncoder

--- AE/a01_autoenoder.py
+++ AE/a01_autoenoder.py
@@ -13,8 +13,8 @@ from tensorflow.keras.datasets import mnist
 # 라벨 값이 필요가 없기에 
 (x_train, _), (x_test, _) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784).astype('float32') / 255.
-x_test = x_test.reshape(10000, 784).astype('float32') / 255.
+x_train = x_train.reshape(60000, 784).astype('float32') / 112.5
+x_test = x_test.reshape(10000, 784).astype('float32') / 122.5
 
 # from keras.models import Sequential # Another Version
 from tensorflow.keras.models import Sequential, Model 
@@ -22,10 +22,14 @@ from tensorflow.keras.layers import Dense, Input
 # 함수형..!
 
 input_img = Input(shape=(784,))
+# 0~1사이 값이 들어온다.
 encoded = Dense(64, activation='relu')(input_img)
-# decoded = Dense(784 ,activation='sigmoid')(encoded) # 첫번째  # x 자체
+decoded = Dense(784 ,activation='sigmoid')(encoded) # 첫번째  # x 자체
 # 375/375 [==============================] - 2s 6ms/step - loss: 0.0729 - acc: 0.0130 - val_loss: 0.0738 - val_acc: 0.0134
-decoded = Dense(784 ,activation='relu')(encoded) # 두번째    # x 자체
+# decoded = Dense(784 ,activation='relu')(encoded) # 두번째    # x 자체
+
+decoded = Dense(784 ,activation='tahn')(encoded) # 첫번째  # x 자체
+
 autoencoder = Model(input_img, decoded)
 # 64 => 784 => 64 => 784 반복
 autoencoder.summary()
@@ -36,7 +40,15 @@ autoencoder.summary()
 #       #
 # 특성을 추출하다가 경계선이 흐릿해지는 현상이 발생할 수 있다.
 
-autoencoder.compile(optimizer = 'adam', loss = 'mse', metrics = ['acc'])
-# autoencoder.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['acc'])
+# autoencoder.compile(optimizer = 'adam', loss = 'mse', metrics = ['acc'])
+# # acc지표는 이곳에서 의미 없다.
+# # sigmoid와 지표 loss를 사용할 수 있다.
+# # autoencoder.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['acc'])
+
+autoencoder.compile(optimizer = 'adam', loss = 'mse')
 
 autoencoder.fit(x_train, x_train, epochs= 30, batch_size = 128, validation_split=0.2) # x로 x를 훈련
+
+# AutoEncoder에서 
+
+# 초창기 GAN에서는 loss를 믿지 말라고 했다.
