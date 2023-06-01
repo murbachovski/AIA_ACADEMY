3d0f639e1d080ed968fa9635fb4a5f677cd10c98
murbachovski <murbachovski@gmail.com>
2023-06-01 11:05:33 +0900

    ae_graph_for

--- AE/a04_ae1_many_graph.py
+++ AE/a04_ae1_many_graph.py
@@ -1,58 +1,78 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist
-import matplotlib.pyplot as plt
-import random
 
-# 1. 데이터 불러오기
+#1. DATA
 (x_train, _), (x_test, _) = mnist.load_data()
 
 x_train = x_train.reshape(60000, 784).astype('float32') / 255
 x_test = x_test.reshape(10000, 784).astype('float32') / 255
 
 # 잡음을 넣는다.
-x_train_noised = x_train + np.random.normal(0, 0.1, size=x_train.shape)
-x_test_noised = x_test + np.random.normal(0, 0.1, size=x_test.shape)
+x_train_noised = x_train + np.random.normal(0, 0.1, size= x_train.shape)
+x_test_noised = x_test + np.random.normal(0, 0.1, size= x_test.shape)
 
 x_train_noised = np.clip(x_train_noised, a_min=0, a_max=1)
 x_test_noised = np.clip(x_test_noised, a_min=0, a_max=1)
 
 
-# 2. 모델 정의
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-
+#2. MODEL
+from tensorflow.keras.models import Sequential, Model 
+from tensorflow.keras.layers import Dense, Input
+# 함수형..!
 def autoencoder(hidden_layer_size):
     model = Sequential()
-    model.add(Dense(units=hidden_layer_size, input_shape=(784,)))
-    model.add(Dense(784, activation='relu'))
+    model.add(Dense(units=hidden_layer_size, input_shape=(784,)))  # Fixed typo here
+    model.add(Dense(784, activation='relu'))    
     return model
 
-models = [
-    autoencoder(hidden_layer_size=1),
-    autoencoder(hidden_layer_size=32),
-    autoencoder(hidden_layer_size=64),
-    autoencoder(hidden_layer_size=154),
-    autoencoder(hidden_layer_size=331)
-]
+model1 = autoencoder(hidden_layer_size=1)
+model32 = autoencoder(hidden_layer_size=32)
+model64 = autoencoder(hidden_layer_size=64)
+model154 = autoencoder(hidden_layer_size=154)
+model331 = autoencoder(hidden_layer_size=331)
+# model486 = autoencoder(hidden_layer_size=486)
+# model713 = autoencoder(hidden_layer_size=713)
 
-# 3. 모델 학습 및 결과 확인
-decoded_imgs_all = []
+# 컴파일 훈련
+model1.compile(optimizer='adam', loss='mse')
+model1.fit(x_train, x_train, epochs=10, batch_size=128)  # Fixed typo here
+# 컴파일 훈련
+model32.compile(optimizer='adam', loss='mse')
+model32.fit(x_train, x_train, epochs=10, batch_size=128) 
+# 컴파일 훈련
+model64.compile(optimizer='adam', loss='mse')
+model64.fit(x_train, x_train, epochs=10, batch_size=128) 
+# 컴파일 훈련
+model154.compile(optimizer='adam', loss='mse')
+model154.fit(x_train, x_train, epochs=10, batch_size=128) 
+# 컴파일 훈련
+model331.compile(optimizer='adam', loss='mse')
+model331.fit(x_train, x_train, epochs=10, batch_size=128) 
 
-for model in models:
-    model.compile(optimizer='adam', loss='mse')
-    model.fit(x_train_noised, x_train_noised, epochs=3, batch_size=128)
+# 평가, 예측
+decoded_imgs1 = model1.predict(x_test_noised)
+# 평가, 예측
+decoded_imgs32 = model32.predict(x_test_noised)
+# 평가, 예측
+decoded_imgs64 = model64.predict(x_test_noised)
+# 평가, 예측
+decoded_imgs154 = model154.predict(x_test_noised)
+# 평가, 예측
+decoded_imgs331 = model331.predict(x_test_noised)
 
-    decoded_imgs = model.predict(x_test_noised)
-    decoded_imgs_all.append(decoded_imgs)
+######################################################################################
 
-fig, axes = plt.subplots(len(decoded_imgs_all), 5, figsize=(15, 15))
+from matplotlib import pyplot as plt
+import random
 
-for i, decoded_imgs in enumerate(decoded_imgs_all):
-    random_images = random.sample(range(decoded_imgs.shape[0]), 5)
-    outputs = [x_test_noised, decoded_imgs]
+fig, axes = plt.subplots(7, 5, figsize=(15, 15))  # Fixed typo here
 
-    for j, ax in enumerate(axes[i]):
-        ax.imshow(outputs[j][random_images[j]].reshape(28, 28), cmap='gray')
+random_images = random.sample(range(decoded_imgs1.shape[0]), 5)
+outputs = [x_test, decoded_imgs1, decoded_imgs32, decoded_imgs64, decoded_imgs154, decoded_imgs331]
+
+for row_num, row in enumerate(axes):
+    for col_num, ax in enumerate(row):
+        ax.imshow(x_test[random_images[row_num]].reshape(28, 28), cmap='gray')
         ax.grid(False)
         ax.set_xticks([])
         ax.set_yticks([])
--- AE/a04_ae2_many_graph.py
+++ AE/a04_ae2_many_graph.py
@@ -1,5 +1,9 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from matplotlib import pyplot as plt
+import random
 
 #1. DATA
 (x_train, _), (x_test, _) = mnist.load_data()
@@ -7,74 +11,45 @@ from tensorflow.keras.datasets import mnist
 x_train = x_train.reshape(60000, 784).astype('float32') / 255
 x_test = x_test.reshape(10000, 784).astype('float32') / 255
 
-# 잡음을 넣는다.
-x_train_noised = x_train + np.random.normal(0, 0.1, size= x_train.shape)
-x_test_noised = x_test + np.random.normal(0, 0.1, size= x_test.shape)
+# Add noise
+x_train_noised = x_train + np.random.normal(0, 0.1, size=x_train.shape)
+x_test_noised = x_test + np.random.normal(0, 0.1, size=x_test.shape)
 
 x_train_noised = np.clip(x_train_noised, a_min=0, a_max=1)
 x_test_noised = np.clip(x_test_noised, a_min=0, a_max=1)
 
-
 #2. MODEL
-from tensorflow.keras.models import Sequential, Model 
-from tensorflow.keras.layers import Dense, Input
-# 함수형..!
 def autoencoder(hidden_layer_size):
     model = Sequential()
-    model.add(Dense(units=hidden_layer_size, input_shape=(784,)))  # Fixed typo here
+    model.add(Dense(units=hidden_layer_size, input_shape=(784,)))
     model.add(Dense(784, activation='relu'))    
     return model
 
-model1 = autoencoder(hidden_layer_size=1)
-model32 = autoencoder(hidden_layer_size=32)
-model64 = autoencoder(hidden_layer_size=64)
-model154 = autoencoder(hidden_layer_size=154)
-model331 = autoencoder(hidden_layer_size=331)
-# model486 = autoencoder(hidden_layer_size=486)
-# model713 = autoencoder(hidden_layer_size=713)
+hidden_layer_sizes = [1, 32, 64, 154, 331]
+models = []
+decoded_imgs = []
 
-# 컴파일 훈련
-model1.compile(optimizer='adam', loss='mse')
-model1.fit(x_train, x_train, epochs=10, batch_size=128)  # Fixed typo here
-# 컴파일 훈련
-model32.compile(optimizer='adam', loss='mse')
-model32.fit(x_train, x_train, epochs=10, batch_size=128) 
-# 컴파일 훈련
-model64.compile(optimizer='adam', loss='mse')
-model64.fit(x_train, x_train, epochs=10, batch_size=128) 
-# 컴파일 훈련
-model154.compile(optimizer='adam', loss='mse')
-model154.fit(x_train, x_train, epochs=10, batch_size=128) 
-# 컴파일 훈련
-model331.compile(optimizer='adam', loss='mse')
-model331.fit(x_train, x_train, epochs=10, batch_size=128) 
+for hidden_size in hidden_layer_sizes:
+    model = autoencoder(hidden_layer_size=hidden_size)
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(x_train, x_train, epochs=2, batch_size=128)
+    models.append(model)
+    decoded_imgs.append(model.predict(x_test_noised))
 
-# 평가, 예측
-decoded_imgs1 = model1.predict(x_test_noised)
-# 평가, 예측
-decoded_imgs32 = model32.predict(x_test_noised)
-# 평가, 예측
-decoded_imgs64 = model64.predict(x_test_noised)
-# 평가, 예측
-decoded_imgs154 = model154.predict(x_test_noised)
-# 평가, 예측
-decoded_imgs331 = model331.predict(x_test_noised)
+fig, axes = plt.subplots(len(hidden_layer_sizes) + 1, 5, figsize=(15, 15))
 
-######################################################################################
-
-from matplotlib import pyplot as plt
-import random
-
-fig, axes = plt.subplots(7, 5, figsize=(15, 15))  # Fixed typo here
-
-random_images = random.sample(range(decoded_imgs1.shape[0]), 5)
-outputs = [x_test, decoded_imgs1, decoded_imgs32, decoded_imgs64, decoded_imgs154, decoded_imgs331]
+random_images = random.sample(range(x_test.shape[0]), 5)
+outputs = [x_test] + decoded_imgs
 
 for row_num, row in enumerate(axes):
     for col_num, ax in enumerate(row):
-        ax.imshow(x_test[random_images[row_num]].reshape(28, 28), cmap='gray')
+        if row_num == 0:
+            ax.imshow(outputs[row_num][random_images[col_num]].reshape(28, 28), cmap='gray')
+        else:
+            ax.imshow(outputs[row_num][random_images[col_num]].reshape(28, 28), cmap='gray')  # Reshape the image
         ax.grid(False)
         ax.set_xticks([])
         ax.set_yticks([])
 
 plt.show()
+
